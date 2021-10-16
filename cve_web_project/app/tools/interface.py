e9d07e60d20a5fbbe09562907d7069374ceeef51
je <wldmsp95@gmail.com>
2021-10-16 16:28:16 +0900

    다운로드까지 합치기

---
+++ cve_web_project/app/tools/interface.py
@@ -0,0 +1,14 @@
+
+BASE = "C:/Users/ParkJiEun/Documents/venv/cve_web_project/app/"
+#BASE = "D:\\PythonProjects\\Insoo\\app"
+
+LOC_MODULES = BASE + "modules/"
+LOC_TMP = BASE + "tmp/"
+
+
+NAME_CODE_C = "{}_CODE.c"
+NAME_CODE_HTML = "{}.html"
+NAME_FILE_BINARY = "{}"
+
+COMPILER_WINDOWS_C = "x86_64-w64-mingw32-gcc"
+COMPILER_LINUX_C = "gcc"
